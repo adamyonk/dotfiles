554a12ec1d8713f21935812d8c223c0f5844a822
Adam Jahnke <adamyonk@me.com>
2011-11-21 06:40:22 -0600

    Adding alias for ejecting disks

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -3,8 +3,10 @@
 #   `brew install coreutils`
 if $(gls &>/dev/null)
 then
-  alias ls="gls -F --color"
-  alias l="gls -lAh --color"
-  alias ll="gls -l --color"
+  alias ls='gls -F --color'
+  alias l='gls -lAh --color'
+  alias ll='gls -l --color'
   alias la='gls -A --color'
 fi
+
+alias eject='hdiutil eject "/Volumes/500GB" && hdiutil eject "/Volumes/Time Machine"'
