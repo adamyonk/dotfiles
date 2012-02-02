4bfe75a14e5ddbaf5844638579514cb3485341a9
Adam Jahnke <adamyonk@me.com>
2012-02-02 09:48:32 -0600

    Aliasing ls to ls -al

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -17,13 +17,15 @@ alias redis-down='killall redis-server'
 
 alias git='nocorrect noglob git'
 
+alias ls='ls -al'
+
 # grc overides for ls
 #   Made possible through contributions from generous benefactors like
 #   `brew install coreutils`
-if $(gls &>/dev/null)
-then
-  alias ls="gls -F --color"
-  alias l="gls -lAh --color"
-  alias ll="gls -l --color"
-  alias la='gls -A --color'
-fi
+#if $(gls &>/dev/null)
+#then
+#  alias ls="gls -F --color"
+#  alias l="gls -lAh --color"
+#  alias ll="gls -l --color"
+#  alias la='gls -A --color'
+#fi
