4a34b3e317b4563590a1d4c0da3feb63327ad9fa
Adam Jahnke <adamyonk@me.com>
2012-02-27 12:33:48 -0600

    Updating vim startup

--- zsh/vim.zsh
+++ zsh/vim.zsh
@@ -1,2 +1,14 @@
 # Set editor
 export EDITOR=vim
+
+# To MacVim or Vim?
+if [[ -x /usr/local/bin/mvim ]]; then
+  if [[ $TERM = 'screen' ]]; then
+    VIM='/usr/local/bin/mvim'
+    VIMFLAGS='-v'
+  else
+    VIM='/usr/local/bin/mvim'
+  fi
+else
+  VIM='vim'
+fi
