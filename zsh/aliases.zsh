fff6d1ff9aee86109ad875e55026f3369372102d
Adam Jahnke <adamyonk@me.com>
2012-04-30 19:41:43 -0500

    Updating fliptable.. Because that's important

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,6 +1,6 @@
 alias reload='source ~/.zshrc'
 alias ps='ps acx -o pid,command'
-alias fliptable='echo （╯°□°）╯︵ ┻━┻'
+alias fliptable='echo "\n（╯°□°）╯︵ ┻━┻\n"'
 
 function colours() {
   for i in {0..255} ; do
