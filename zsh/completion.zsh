2f6a222c3e64a42120d2ea7d3f71540e65b513f2
Adam Jahnke <adamyonk@me.com>
2012-05-15 12:20:09 -0500

    Removing unused functions

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -3,3 +3,10 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 # Pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
+
+# Verbose complettion
+zstyle ':completion:*' verbose yes
+zstyle ':completion:*:descriptions' format '%B%d%b'
+zstyle ':completion:*:messages' format '%d'
+zstyle ':completion:*:warnings' format 'No matches for: %d'
+zstyle ':completion:*' group-name ''
