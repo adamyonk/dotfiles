c1f5f14f28d088c1bd756bc8b7e6d87708a69be9
Adam Jahnke <adamyonk@me.com>
2012-06-20 14:18:22 -0500

    Small tweaks for ubuntu compatibility

--- system/grc.zsh
+++ system/grc.zsh
@@ -1,2 +1,9 @@
-# Grab grc if it's there
-if [[ -f `command -v grc` ]] ; then source $grc_path/etc/grc.bashrc ; fi
+# Check for brew and grc
+if [[ -f `command -v brew` ]]
+then
+  grc_path=$(brew --prefix grc)
+  if [[ -f $grc_path ]]
+  then
+    source $grc_path/etc/grc.bashrc
+  fi
+fi
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -7,9 +7,9 @@ function colours() {
 alias e=$EDITOR
 alias fliptable='echo "\n（╯°□°）╯︵ ┻━┻\n"'
 alias ls='ls -al' # Include dot files, list in long format
+#alias minecraft='cd ~/.minecraft && java -Xmx1024M -Xms1024M -jar minecraft_server.jar nogui'
+alias minecraft='cd ~/.minecraft && java -jar minecraft_server.jar nogui'
 alias ps='ps -a -c -o pid,command -x' # All users processes, only executable name, custom output, show processes with no controlling terminal
 alias reload='source ~/.zshrc'
 alias rup='systemsetup -setremotelogin on'
 alias rdown='systemsetup -setremotelogin off'
-alias minecraft='cd ~/.minecraft && java -Xmx1024M -Xms1024M -jar minecraft_server.jar nogui'
-# 138/65
