64011921c526fb2480130b762f833f7c1f961147
Adam Jahnke <adamyonk@me.com>
2012-07-06 14:50:19 -0500

    Better documentation for my zsh config

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -16,34 +16,32 @@ HISTFILE=~/.zsh_history
 HISTSIZE=10000
 SAVEHIST=10000
 
-setopt NO_BG_NICE # Don't nice background tasks
-setopt NO_HUP
-setopt NO_LIST_BEEP
-setopt LOCAL_OPTIONS # Allow functions to have local options
-setopt LOCAL_TRAPS # Allow functions to have local traps
-setopt HIST_VERIFY
-setopt SHARE_HISTORY # Share history between sessions ???
-setopt EXTENDED_HISTORY # Add timestamps to history
-setopt PROMPT_SUBST
-setopt CORRECT
-setopt COMPLETE_IN_WORD
-setopt IGNORE_EOF
+setopt append_history # Adds history instead of replacing
+setopt no_bg_nice # Don't nice background tasks
+setopt complete_aliases # Don't expand aliases _before_ completion has finished, like: git comm-[tab]
+setopt complete_in_word # Complete from both ends when inside word
+setopt correct # Try to correct commands
+setopt no_correct_all # Don't correct arguments
+setopt extended_history # Add timestamps to history
+setopt hist_ignore_all_dups # Don't record dupes in history
+setopt hist_reduce_blanks # Remove superfluous blanks
+setopt hist_verify # Load lines from history expansion nito editing buffer
+setopt no_hup # Send the HUP signal to running jobs when the shell exits
+setopt ignore_eof # Don't exit on EOF
+setopt inc_append_history # Add lines to history as soon as they're added
+setopt no_list_beep # Beep on an ambiguous completion
+setopt local_options # Allow functions to have local options
+setopt local_traps # Allow functions to have local traps
+setopt prompt_subst # Parameter expansion, command substitution, arithmetic expansion performed in prompts
+setopt share_history # Adds history incrementally and share it across sessions
 
-setopt APPEND_HISTORY # Adds history
-setopt INC_APPEND_HISTORY SHARE_HISTORY # Adds history incrementally and share it across sessions
-setopt HIST_IGNORE_ALL_DUPS # Don't record dupes in history
-setopt HIST_REDUCE_BLANKS
 
-# Don't expand aliases _before_ completion has finished
-#   like: git comm-[tab]
-setopt complete_aliases
+# Vi-mode. Borrowign this from oh-my-zsh
+function zle-line-init zle-keymap-select {
+  zle reset-prompt
+}
 
-zle -N newtab
+zle -N zle-line-init
+zle -N zle-keymap-select
 
-bindkey '^[^[[D' backward-word
-bindkey '^[^[[C' forward-word
-bindkey '^[[5D' beginning-of-line
-bindkey '^[[5C' end-of-line
-bindkey '^[[3~' delete-char
-bindkey '^[^N' newtab
-bindkey '^?' backward-delete-char
+bindkey -v
