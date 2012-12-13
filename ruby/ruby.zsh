b15abe247bbf3ed2aa0ba3cd2960ec050a3d001b
Adam Jahnke <adamyonk@me.com>
2012-12-13 10:26:59 -0600

    Updates some ruby runtime vars

--- ruby/ruby.zsh
+++ ruby/ruby.zsh
@@ -26,8 +26,9 @@ alias nginx-down="sudo nginx -s stop"
 alias redis-up='redis-server /usr/local/etc/redis.conf > /dev/null &'
 alias redis-down='killall redis-server'
 
-export RUBY_HEAP_MIN_SLOTS=1000000
-export RUBY_HEAP_SLOTS_INCREMENT=1000000
-export RUBY_HEAP_SLOTS_GROWTH_FACTOR=1
-export RUBY_GC_MALLOC_LIMIT=1000000000
-export RUBY_HEAP_FREE_MIN=500000
+# This might not be good to have on non-dev machines
+#export RUBY_HEAP_MIN_SLOTS=1000000
+#export RUBY_HEAP_SLOTS_INCREMENT=1000000
+#export RUBY_HEAP_SLOTS_GROWTH_FACTOR=1
+#export RUBY_GC_MALLOC_LIMIT=1000000000
+#export RUBY_HEAP_FREE_MIN=500000
