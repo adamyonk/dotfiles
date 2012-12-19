5d15b47b1a11b7b61e6dd7750d39d96d93d7a509
Adam Jahnke <adamyonk@me.com>
2012-12-19 15:07:18 -0600

    Minor updates

--- ruby/ruby.zsh
+++ ruby/ruby.zsh
@@ -11,20 +11,13 @@ function bbu() {
   bin/unicorn -E development -c /usr/local/etc/unicorn.rb -1 127.0.0.1:$UNICORN_PORT
 }
 
-alias pc_precompile='bundle exec rake assets:precompile RAILS_ENV=development RAILS_ASSETS_NODIGEST=true'
-
-alias bundle='nocorrect bundle'
+alias rake='noglob rake'
+alias bundle='noglob bundle'
 alias be='bundle exec'
 alias bi='bundle install'
 alias fr='bundle exec foreman run bundle exec'
 
 #alias killall=exec "kill -QUIT $(ps aux | grep "postgres\|redis\|mailcatcher\|sphinx\|unicorn" | awk '{print $2}' | xargs)"
-
-alias pg-up='pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log'
-alias nginx-up="sudo nginx -c /usr/local/etc/nginx/nginx.conf"
-alias nginx-down="sudo nginx -s stop"
-alias redis-up='redis-server /usr/local/etc/redis.conf > /dev/null &'
-alias redis-down='killall redis-server'
 
 # This might not be good to have on non-dev machines
 #export RUBY_HEAP_MIN_SLOTS=1000000
@@ -32,3 +25,10 @@ alias redis-down='killall redis-server'
 #export RUBY_HEAP_SLOTS_GROWTH_FACTOR=1
 #export RUBY_GC_MALLOC_LIMIT=1000000000
 #export RUBY_HEAP_FREE_MIN=500000
+
+alias pg-up='pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log'
+alias nginx-up="sudo nginx -c /usr/local/etc/nginx/nginx.conf"
+alias nginx-down="sudo nginx -s stop"
+alias redis-up='redis-server /usr/local/etc/redis.conf > /dev/null &'
+alias redis-down='killall redis-server'
+alias pc_precompile='bundle exec rake assets:precompile RAILS_ENV=development RAILS_ASSETS_NODIGEST=true'
