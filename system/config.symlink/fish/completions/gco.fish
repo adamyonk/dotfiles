4e440d868f22b4ef0951b681e732276777c680c1
Adam Jahnke <adamyonk@me.com>
2013-03-11 09:15:00 -0500

    Update fish completions

--- system/config.symlink/fish/completions/gco.fish
+++ system/config.symlink/fish/completions/gco.fish
@@ -1,4 +1,4 @@
-set -l arguments (git branch --all --no-color | awk '!/^*|HEAD/ { printf "%s ", $1 }')
+set -l arguments (git branch --all --no-color | awk '!/^*|HEAD/ { printf "%s ", $1 }' | sed s_remotes/__)
 
 complete --arguments "$arguments" --command gco --no-files --require-parameter
 complete --command gco --long-option track --short-option t
--- system/config.symlink/fish/completions/gcot.fish
+++ system/config.symlink/fish/completions/gcot.fish
@@ -1,3 +1,3 @@
-set -l arguments (git branch --all --no-color | awk '!/^*|HEAD/ && /remotes/ { printf "%s ", $1 }')
+set -l arguments (git branch --all --no-color | awk '!/^*|HEAD/ && /remotes/ { printf "%s ", $1 }' | sed s_remotes/__)
 
 complete --arguments "$arguments" --command gcot --no-files --require-parameter
