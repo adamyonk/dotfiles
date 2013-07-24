77d54d2a000acd7662e0b2432e83624b418faa9f
Adam Jahnke <adamyonk@me.com>
2013-07-24 10:02:30 -0500

    Update bash prompt

--- bash/prompt.bash
+++ bash/prompt.bash
@@ -4,7 +4,7 @@ color_red=$'\033[0;31m'
 color_none=$'\033[0m'
 
 # Last status
-prompt_status() {
+_prompt_status() {
   status="$?"
   if [ "$status" == 0 ]; then
     printf "$color_green"
@@ -16,12 +16,12 @@ prompt_status() {
 }
 
 # Working directory
-prompt_dir() {
+_prompt_dir() {
   printf "${PWD##*/}"
 }
 
 # Git info
-prompt_git() {
+_prompt_git() {
   if git status >/dev/null 2>&1; then
     if git diff --quiet HEAD 2>&1; then
       local color="$color_green"
@@ -33,10 +33,10 @@ prompt_git() {
 }
 
 # Prompt char for user or root
-prompt_char() {
+_prompt_char() {
   local char=➔
   [ "$UID" == 0 ] && char=\$
   printf "$char"
 }
 
-PS1='\[\033[G\] $(prompt_status) $(prompt_dir)$(prompt_git) $(prompt_char) '
+PS1='\[\033[G\] $(_prompt_status) $(_prompt_dir)$(_prompt_git) $(_prompt_char) '
