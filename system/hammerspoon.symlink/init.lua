3ab1ca865c7fe00601e80c3474d361de11b523e8
Adam Jahnke <adamyonk@me.com>
2016-12-16 10:09:24 -0600

    Add karabiner/hammerspoon

---
+++ system/hammerspoon.symlink/init.lua
@@ -0,0 +1,4 @@
+require('better-caps-lock')
+require('hyper')
+
+hs.notify.new({title="Hammerspoon", informativeText="Ready to rock 🤘"}):send()
---
+++ system/hammerspoon.symlink/windows.lua
@@ -0,0 +1,105 @@
+hs.window.animationDuration = 0
+
+-- +-----------------+
+-- |        |        |
+-- |  HERE  |        |
+-- |        |        |
+-- +-----------------+
+function hs.window.left(win)
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end
+
+-- +-----------------+
+-- |        |        |
+-- |        |  HERE  |
+-- |        |        |
+-- +-----------------+
+function hs.window.right(win)
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x + (max.w / 2)
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end
+
+-- +-----------------+
+-- |      HERE       |
+-- +-----------------+
+-- |                 |
+-- +-----------------+
+function hs.window.up(win)
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.w = max.w
+  f.y = max.y
+  f.h = max.h / 2
+  win:setFrame(f)
+end
+
+-- +-----------------+
+-- |                 |
+-- +-----------------+
+-- |      HERE       |
+-- +-----------------+
+function hs.window.down(win)
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.w = max.w
+  f.y = max.y + (max.h / 2)
+  f.h = max.h / 2
+  win:setFrame(f)
+end
+
+-- +-----------------+
+-- |  HERE  |        |
+-- +--------+        |
+-- |                 |
+-- +-----------------+
+function hs.window.upLeft(win)
+  -- TODO
+end
+
+-- +-----------------+
+-- |                 |
+-- +--------+        |
+-- |  HERE  |        |
+-- +-----------------+
+function hs.window.downLeft(win)
+  -- TODO
+end
+
+-- +-----------------+
+-- |                 |
+-- |        +--------|
+-- |        |  HERE  |
+-- +-----------------+
+function hs.window.downRight(win)
+  -- TODO
+end
+
+-- +-----------------+
+-- |        |  HERE  |
+-- |        +--------|
+-- |                 |
+-- +-----------------+
+function hs.window.upRight(win)
+  -- TODO
+end
