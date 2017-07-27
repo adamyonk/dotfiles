d431b0578e494012d11ea8e961c132e1d5f9faa3
Adam Jahnke <adamyonk@me.com>
2017-07-27 15:29:28 -0500

    Update bash settings/prompt

--- bash/prompt.bash
+++ bash/prompt.bash
@@ -1,57 +1,87 @@
-# Prompt
-color_green=$'\033[0;32m'
-color_red=$'\033[0;31m'
-color_none=$'\033[0;m'
+#
+# Clean and minimalistic Bash prompt
+# Author: Artem Sapegin, sapegin.me
+# 
+# Inspired by: https://github.com/sindresorhus/pure & https://github.com/dreadatour/dotfiles/blob/master/.bash_profile
+#
+# Notes:
+# - $local_username - username you don’t want to see in the prompt - can be defined in ~/.bashlocal : `local_username="admin"`
+# - Colors ($RED, $GREEN) - defined in ../tilde/bash_profile.bash
+#
 
-# Last status
-_prompt_status() {
-  status="$?"
-  if [ "$status" == 0 ]; then
-    printf "$color_green"
-  else
-    printf "$color_red"
-  fi
-  printf '⤫'
-  printf "$color_none"
+
+# User color
+case $(id -u) in
+	0) user_color="$RED" ;;  # root
+	*) user_color="$GREEN" ;;
+esac
+
+# Symbols
+prompt_symbol="❯"
+prompt_clean_symbol="☀ "
+prompt_dirty_symbol="☂ "
+prompt_venv_symbol="☁ "
+
+function prompt_command() {
+	# Local or SSH session?
+	local remote=
+	[ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ] && remote=1
+
+	# Git branch name and work tree status (only when we are inside Git working tree)
+	local git_prompt=
+	if [[ "true" = "$(git rev-parse --is-inside-work-tree 2>/dev/null)" ]]; then
+		# Branch name
+		local branch="$(git symbolic-ref HEAD 2>/dev/null)"
+		branch="${branch##refs/heads/}"
+
+		# Working tree status (red when dirty)
+		local dirty=
+		# Modified files
+		git diff --no-ext-diff --quiet --exit-code --ignore-submodules 2>/dev/null || dirty=1
+		# Untracked files
+		[ -z "$dirty" ] && test -n "$(git status --porcelain)" && dirty=1
+
+		# Format Git info
+		if [ -n "$dirty" ]; then
+			git_prompt=" $RED$prompt_dirty_symbol$branch$NOCOLOR"
+		else
+			git_prompt=" $GREEN$prompt_clean_symbol$branch$NOCOLOR"
+		fi
+	fi
+
+	# Virtualenv
+	local venv_prompt=
+	if [ -n "$VIRTUAL_ENV" ]; then
+	    venv_prompt=" $BLUE$prompt_venv_symbol$(basename $VIRTUAL_ENV)$NOCOLOR"
+	fi
+
+	# Only show username if not default
+	local user_prompt=
+	[ "$USER" != "$local_username" ] && user_prompt="$user_color$USER$NOCOLOR"
+
+	# Show hostname inside SSH session
+	local host_prompt=
+	[ -n "$remote" ] && host_prompt="@$YELLOW$HOSTNAME$NOCOLOR"
+
+	# Show delimiter if user or host visible
+	local login_delimiter=
+	[ -n "$user_prompt" ] || [ -n "$host_prompt" ] && login_delimiter=":"
+
+	# Format prompt
+	first_line="$user_prompt$host_prompt$login_delimiter$WHITE\w$NOCOLOR$git_prompt$venv_prompt"
+	# Text (commands) inside \[...\] does not impact line length calculation which fixes stange bug when looking through the history
+	# $? is a status of last command, should be processed every time prompt prints
+	second_line="\`if [ \$? = 0 ]; then echo \[\$CYAN\]; else echo \[\$RED\]; fi\`\$prompt_symbol\[\$NOCOLOR\] "
+	PS1="\n$first_line\n$second_line"
+
+	# Multiline command
+	PS2="\[$CYAN\]$prompt_symbol\[$NOCOLOR\] "
+
+	# Terminal title
+	local title="$(basename "$PWD")"
+	[ -n "$remote" ] && title="$title \xE2\x80\x94 $HOSTNAME"
+	echo -ne "\033]0;$title"; echo -ne "\007"
 }
 
-# Working directory
-_prompt_dir() {
-  printf "${PWD##*/}"
-}
-
-# Git info
-_prompt_git() {
-  if git status >/dev/null 2>&1; then
-    if git diff --quiet HEAD 2>&1; then
-      local color="$color_green"
-    else
-      local color="$color_red"
-    fi
-    printf " $color$(git branch | awk '/\*/ {print $2}')$color_none"
-  fi
-}
-
-# Prompt char for user or root
-_prompt_char() {
-  local char=➔
-  [ "$UID" == 0 ] && char=\$
-  printf "$char"
-}
-
-set_ps1() {
-  PS1='\n\[\033[G\]$(_prompt_status) $(_prompt_dir)$(_prompt_git)\n$(_prompt_char) '
-}
-
-# Prompt command
-prompt_command() {
-  # Only run this if we have a .git directory
-  #[[ -d .git ]] &&
-  # If we just ran `git status`, don't run it again
-  #[[ `history 1` != *'g s'* ]] &&
-  # Don't show every untracked file in a directory
-  #git status --branch --short --untracked=normal
-  set_ps1
-}
-
-PROMPT_COMMAND='prompt_command'
+# Show awesome prompt only if Git is istalled
+command -v git >/dev/null 2>&1 && PROMPT_COMMAND=prompt_command
