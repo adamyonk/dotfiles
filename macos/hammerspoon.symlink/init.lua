fed02ee3629dba0ac1734a2fefe8869480d7fbf8
Adam Jahnke <adamyonk@me.com>
2017-11-26 13:58:27 -0600

    Rearrange some hammerspoon things

--- macos/hammerspoon.symlink/init.lua
+++ macos/hammerspoon.symlink/init.lua
@@ -1,2 +1,12 @@
-require('keyboard') -- Load Hammerspoon bits from https://github.com/jasonrudolph/keyboard
+-- require('keyboard') -- Load Hammerspoon bits from https://github.com/jasonrudolph/keyboard
+
+hs.hotkey.bind({'ctrl'}, '`', nil, function()
+  hs.reload()
+end)
+
+hs.loadSpoon('ControlEscape')
+hs.loadSpoon('Markdown')
 hs.loadSpoon('PushToTalk')
+hs.loadSpoon('WindowLayout')
+
+hs.notify.new({title='Hammerspoon', informativeText='Ready to rock 🤘'}):send()
