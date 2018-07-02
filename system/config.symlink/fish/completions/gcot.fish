d2f0d525af0aa7d30d66dafcc00d36a871e672db
Adam Jahnke <adamyonk@me.com>
2018-07-02 09:56:44 -0500

    Don't include ~/.config in dotfiles

--- system/config.symlink/fish/completions/gcot.fish
+++
@@ -1,3 +0,0 @@
-set -l arguments (git branch --all --no-color | awk '!/^*|HEAD/ && /remotes/ { printf "%s ", $1 }' | sed s_remotes/__)
-
-complete --arguments "$arguments" --command gcot --no-files --require-parameter
--- system/config.symlink/fish/completions/mx.fish
+++
@@ -1,3 +0,0 @@
-set -l arguments (ls -l $PROJECTS | grep '^d' | awk '{printf $NF" "}')
-
-complete --arguments "$arguments" --command mx --no-files
